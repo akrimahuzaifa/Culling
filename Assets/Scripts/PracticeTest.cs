de952cf87b1f3476e7bc1cb13b0a95546d093cb9
Akrima Huzaifa Akhtar <akrimaamirka@gmail.com>
2022-06-23 11:13:47 +0500

    Questions added

--- Assets/Scripts/PracticeTest.cs
+++ Assets/Scripts/PracticeTest.cs
@@ -15,7 +15,7 @@ public class PracticeTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -34,7 +34,7 @@ public class PracticeTest : MonoBehaviour
         int ySumOfOdd = 0;
         for (int index = 0; index < a.Length; index++)
         {
-            if(a[index] % 2 == 0)
+            if (a[index] % 2 == 0)
             {
                 xSumOfEven += a[index];
             }
@@ -44,5 +44,61 @@ public class PracticeTest : MonoBehaviour
             }
         }
         return ySumOfOdd - xSumOfEven;
+    }
+
+    int[] array = new int[] { 2, 3, 5, 7, 11 };                     //1
+    int[] array1 = new int[] { 2, 3, 6, 7, 11 };                    //0
+    int[] array2 = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10 };        //0
+    int[] array3 = new int[] { 2, 4, 8, 16, 32 };                   //1
+    int[] array4 = new int[] { 3, 9, 27, 7, 1, 1, 1, 1, 1 };        //1
+    int[] array5 = new int[] { 7, 11, 77, 49 };                     //1
+    int[] array6 = new int[] { 2 };                                 //1
+    int[] array7 = new int[] { };                                   //1
+    int[] array8 = new int[] { 7, 2, 7, 2, 7, 2, 7, 2, 3, 7, 7 };   //1
+
+
+
+    public void callFunc()
+    {
+        Debug.Log(is235Array(array8));
+
+    }
+
+    public int is235Array(int[] a)
+    {
+        int isDivivdedByTwo = 0;
+        int isDivivdedByThree = 0;
+        int isDivivdedByFive = 0;
+
+        int unableToDivide = 0;
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] % 2 == 0 || a[i] % 3 == 0 || a[i] % 5 == 0)
+            {
+                if (a[i] % 2 == 0)
+                {
+                    isDivivdedByTwo++;
+                }
+                    
+                if (a[i] % 3 == 0)
+                {
+                    isDivivdedByThree++;
+                }
+                    
+                if (a[i] % 5 == 0)
+                {
+                    isDivivdedByFive++;
+                }    
+            }
+            else
+            {
+                unableToDivide++;
+            }
+        }
+
+        if (isDivivdedByTwo + isDivivdedByThree + isDivivdedByFive + unableToDivide == a.Length)
+            return 1;
+        return 0;
     }
 }
